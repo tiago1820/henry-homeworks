274f86fd1469903a9ed6ca293e24ee07c374f626
tiago1820 <tiago.zdo@gmail.com>
2023-09-21 02:08:13 -0300

    My first Redux studies.

---
+++ M2/11-redux/src/index.js
@@ -0,0 +1,21 @@
+const { createStore } = require("redux");
+const contadorReducer = require("./reducer");
+const { incremento, decremento } = require('./actions');
+
+const store = createStore(contadorReducer);
+
+const valor = document.querySelector('#valor');
+
+function renderContador() {
+  let counter = store.getState().contador;
+  valor.innerHTML = counter;
+}
+
+renderContador();
+
+store.subscribe(renderContador);
+
+const incrementoBtn = document.querySelector('#incremento');
+const decrementoBtn = document.querySelector('#decremento');
+incrementoBtn.addEventListener('click', () => store.dispatch(incremento()));
+decrementoBtn.addEventListener('click', () => store.dispatch(decremento()));
