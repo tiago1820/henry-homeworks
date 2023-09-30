f3bacbfab57fc70970daaff5af0a9a32ba700825
tiago1820 <tiago.zdo@gmail.com>
2023-09-30 07:03:24 -0300

    Updated source code of 13-React Hooks class integrator project

---
+++ M2/pi-paso-paso/07-reac-hooks/src/components/Favorites/Favorites.jsx
@@ -0,0 +1,62 @@
+import { connect, useDispatch } from "react-redux";
+import Card from "../Card/Card";
+import { filterCards, orderCards } from "../../redux/actions";
+import { useState } from "react";
+
+const Favorites = (props) => {
+    const dispatch = useDispatch();
+    const { myFavorites } = props;
+
+    const [aux, setAux] = useState(false);
+
+    const handleOrder = (e) => {
+        dispatch(orderCards(e.target.value));
+        setAux(!aux)
+    };
+
+    const handleFilter = (e) => {
+        dispatch(filterCards(e.target.value));
+    };
+
+    return (
+        <div>
+            <select onChange={handleOrder}>
+                <option value="A">Ascendente</option>
+                <option value="B">Descendente</option>
+            </select>
+
+            <select onChange={handleFilter}>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Gender">Gender</option>
+                <option value="unknown">unknown</option>
+            </select>
+
+            {
+                myFavorites.map((char) => {
+                    return (
+                        <Card
+                            key={char.id}
+                            id={char.id}
+                            name={char.name}
+                            status={char.status}
+                            species={char.species}
+                            gender={char.gender}
+                            origin={char.origin.name}
+                            image={char.image}
+
+                        />
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => {
+    return {
+        myFavorites: state.myFavorites,
+    }
+}
+
+export default connect(mapStateToProps, null)(Favorites);
