20e88202ac9e987004d40e7e6e8ddf229619de3a
tiago1820 <tiago.zdo@gmail.com>
2023-10-10 20:11:08 -0300

    Task 02-Web server done

---
+++ M3/02-WebServer/Client/src/components/About/About.jsx
@@ -0,0 +1,10 @@
+
+const About = () => {
+  return (
+    <div>
+      About here
+    </div>
+  )
+}
+
+export default About
---
+++ M3/02-WebServer/Client/src/components/Favorites/Favorites.jsx
@@ -0,0 +1,67 @@
+import { connect, useDispatch } from "react-redux";
+import Card from "../Card/Card";
+import { filterCards, orderCards, showAllcharacters } from "../../redux/actions";
+import { useState } from "react";
+
+const Favorites = (props) => {
+    const dispatch = useDispatch();
+    const { myFavorites } = props;
+
+    const [aux, setAux] = useState(false);
+
+    const handleOrder = (e) => {
+        dispatch(orderCards(e.target.value));
+        setAux(!aux);
+    }
+
+    const handleFilter = (e) => {
+        const selectedValue = e.target.value;
+        if(selectedValue === "All") {
+            dispatch(showAllcharacters());
+        } else {
+            dispatch(filterCards(selectedValue));
+        }
+    }
+
+    return (
+        <div>
+
+            <select onChange={handleOrder}>
+                <option value="A">Ascendente</option>
+                <option value="D">Descendente</option>
+            </select>
+
+            <select onChange={handleFilter}>
+                <option value="All">All</option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Genderless">Genderless</option>
+                <option value="unknown">unknown</option>
+            </select>
+
+            {myFavorites.map((char) => {
+                return (
+                    <Card
+                        key={char.id}
+                        id={char.id}
+                        name={char.name}
+                        status={char.status}
+                        species={char.species}
+                        gender={char.gender}
+                        origin={char.origin.name}
+                        image={char.image}
+
+                    />
+                )
+            })}
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => {
+    return {
+        myFavorites: state.myFavorites,
+    }
+}
+
+export default connect(mapStateToProps, null)(Favorites);
